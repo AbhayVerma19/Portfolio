ddf6930b60f3c6ca0b12c42d94bb54c4428d3411
Abhay Verma <abhver192002@gmail.com>
2024-10-19 00:59:07 +0530

    Corrected project ID in sanity/sanity.client.ts file

--- portfoliok/sanity/sanity.client.ts
+++ portfoliok/sanity/sanity.client.ts
@@ -3,7 +3,7 @@
 import { createClient, type ClientConfig } from "@sanity/client";
 
 const config: ClientConfig = {
-  projectId: "ga8lllhf",
+  projectId: "z8itxxhs",
   dataset: "production",
   apiVersion: "2024-10-17",
   useCdn: false,
